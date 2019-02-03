9f0d45c3404f39ccd2ef955d10189f32dda3ffbe
sologuboved <sologuboved@gmail.com>
2019-02-03 17:12:03 +0300

    wip

--- adder.py
+++ adder.py
@@ -39,5 +39,6 @@ def duplicate_entry(source_json, target_json=None, **fields):
 
 
 if __name__ == '__main__':
-    add_fields(LJ_POEMS_JSON, **{WHERE: str()})
-    # duplicate_entry(LJ_POEMS_JSON, **{SOURCE: 'https://aconite26.livejournal.com/169053.html'})
+    # add_fields(LJ_POEMS_JSON, **{IS_SONG: False, IS_DERIVATIVE: False})
+    # duplicate_entry(LJ_POEMS_JSON, **{SOURCE: 'https://aconite26.livejournal.com/9632.html'})
+    pass
